9755edcb7f3c878b770a157083a37992574d1acb
Ian Nelson <ian@ianfnelson.com>
2014-05-03 22:52:28 +0100

    stats

--- FHEnhancer/PageBuilder.cs
+++ FHEnhancer/PageBuilder.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Configuration;
 using System.IO;
+using HtmlAgilityPack;
 
 namespace FHEnhancer
 {
@@ -25,9 +26,40 @@ namespace FHEnhancer
             template = template.Replace("{{CURRENT_YEAR}}", now.Year.ToString());
             template = template.Replace("{{LAST_UPDATED}}", now.ToString("dd MMMM yyyy"));
 
-            // TODO - stats - pagecount, picture count, person count
+            var stats = GetStats();
+
+            template = template.Replace("{{PAGE_COUNT}}", stats.Pages);
+            template = template.Replace("{{PERSON_COUNT}}", stats.People);
+            template = template.Replace("{{PICTURE_COUNT}}", stats.Pictures);
 
             return template;
+        }
+
+        private static Stats GetStats()
+        {
+            var sourceDir = ConfigurationManager.AppSettings["SourceDirectory"];
+
+            var statsDoc = new HtmlDocument();
+            statsDoc.Load(Path.Combine(sourceDir, "_statistics.html"));
+
+            var statsNode = statsDoc.DocumentNode.SelectSingleNode("//div[contains(@class,'FhStatsData')]");
+            var pagesRow = statsNode.SelectSingleNode("//table/tr[1]");
+            var peopleRow = statsNode.SelectSingleNode("//table/tr[2]");
+            var picturesRow = statsNode.SelectSingleNode("//table/tr[3]");
+
+            var stats = new Stats
+            {
+                Pages = GetStatFigure(pagesRow),
+                People = GetStatFigure(peopleRow),
+                Pictures = GetStatFigure(picturesRow)
+            };
+
+            return stats;
+        }
+
+        private static string GetStatFigure(HtmlNode statRow)
+        {
+            return statRow.ChildNodes[1].InnerText;
         }
 
         public string BuildPage(string title, string content, string fileName)
@@ -39,6 +71,15 @@ namespace FHEnhancer
             // TODO - advert
 
             return page;
+        }
+
+        public class Stats
+        {
+            public string Pages { get; set; }
+
+            public string People { get; set; }
+
+            public string Pictures { get; set; }
         }
     }
 }
